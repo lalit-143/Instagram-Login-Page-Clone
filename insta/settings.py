e20633481d1530b7b20a15ee22e3455f93c14994
lalit-143 <82702053+lalit-143@users.noreply.github.com>
2022-11-13 15:47:48 +0530

    .

--- insta/settings.py
+++ insta/settings.py
@@ -115,14 +115,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-WHITENOISE_USE_FINDERS = True
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 STATIC_URL = 'static/'
 
 if DEBUG:
@@ -136,8 +128,3 @@ else:
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
